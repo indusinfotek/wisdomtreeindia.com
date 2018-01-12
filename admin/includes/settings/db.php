8b522ae697346baa82ca3613ef42c5c3c0bf0c2b
Abhishek Tiwari <abhishekk.tiwari@outlook.com>
2018-01-13 01:11:07 +0530

    13.01.2018
    13.01.2018

--- admin/includes/settings/db.php
+++ admin/includes/settings/db.php
@@ -14,9 +14,9 @@ class db {
     //public function db($args = array()) {
     public function db() {
         $this->db['server']     = 'localhost';
-        $this->db['username'] 	= 'abworzlc_usrAlwa';
-        $this->db['password'] 	= 'BGhqUHz)22a~';
-        //$this->db['database'] 	= $args[3];             
+        $this->db['username'] 	= 'root';
+        $this->db['password'] 	= '';
+        //$this->db['database'] 	= $args[3];
     }
 
     public function connect() {
@@ -26,7 +26,7 @@ class db {
 
     public function disconnect() {
         mysqli_close($this->connection);
-        $this->connection = null;        
+        $this->connection = null;
     }
 
     public function select_db($dbname) {
@@ -35,12 +35,12 @@ class db {
     }
 
     public function query($sql) {
-        $this->result = mysqli_query($this->connection, $sql);   
+        $this->result = mysqli_query($this->connection, $sql);
         return $this->result;
     }
 
     public function is_connected() {
-            return ($this->connection) ? true : false;            
+            return ($this->connection) ? true : false;
     }
 
     public function clean($dirty) {
@@ -48,15 +48,15 @@ class db {
                 $dirty = ereg_replace("[\'\")(;|`,<>]", "", $dirty);
                 $dirty = mysqli_real_escape_string($this->connection, trim($dirty));
                 $clean = stripslashes($dirty);
-                return $clean;                    
+                return $clean;
         }
         $clean = array();
         foreach ($dirty as $p=>$data) {
                 $data = ereg_replace("[\'\")(;|`,<>]", "", $data);
                 $data = mysqli_real_escape_string($this->connection, trim($data));
                 $data = stripslashes($data);
-                $clean[$p] = $data;                    
+                $clean[$p] = $data;
         }
-        return $clean;   
+        return $clean;
     }
 }
