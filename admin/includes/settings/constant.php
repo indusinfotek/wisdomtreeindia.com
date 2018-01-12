88d2464015d05b3154042bc6e1171d3ea0894ada
Abhishek Tiwari <abhishekk.tiwari@outlook.com>
2018-01-13 00:58:53 +0530

    13.01.2017
    13.01.2017

---
+++ admin/includes/settings/constant.php
@@ -0,0 +1,9 @@
+<?php
+    $basePath       = 'http://'.$_SERVER['SERVER_NAME'].'/';
+    $adminbasePath  = $basePath.'__admin/';
+    define('ADMINMAIL', 'admin_wisdomtree@abworks.in');
+    define('MAINDB', 'dbWisdomTreeIndia');
+    define('SITE_TITLE', 'Wisdom Tree India');
+    define('SITE_TAG', 'Self help books India, Yoga Books  India, Mind, Body, Spirit Books India, Art & Culture Books India, Children Books India, Health Books  India, Motivation Books  India, Spirituality Books India');
+    define('SITE_DESCRIPTION', 'At Wisdomtreeindia, you can find Books on Indian Art & Culture, Yoga & Health, Self Help & for Children, sprituality, motivation, health and many more.');
+?>
--- home.php
+++ home.php
@@ -1,3 +1,9 @@
 <?php
-    echo '<h1>Welcome! to Home Page</h1>';
+    echo '<h1>Welcome! to Home Page.</h1>';
 ?>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body></body>
+</html>
